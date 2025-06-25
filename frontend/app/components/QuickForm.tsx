1af84fabea751d1364089da018b445a98cf8ecf1
entrpyc <contact@a-angelov.eu>
2025-06-25 17:12:20 +0200

    update quickform

--- frontend/app/components/QuickForm.tsx
+++ frontend/app/components/QuickForm.tsx
@@ -5,37 +5,23 @@ import { useRef } from 'react';
 import {
   Button,
   Card,
-  Divider,
-  FileInput,
   Flex,
   Loader,
   Paper,
-  SegmentedControl,
   Select,
-  Slider,
   Stack,
   Text,
   TextInput,
   Title
 } from '@mantine/core';
-import { DateInput } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import { notifications } from '@mantine/notifications';
-import { downloadFile, formatDate, formatTitle } from '../utils/helpers';
 import { ZoomRecording } from '../types/types';
 import css from '../style/styles.module.css';
 
-const TELEGRAM_OIL_OF_GLADNESS_ID = '-1001388987517'
-const TELEGRAM_SHEAF_YARD_GROUP_ID = '-10013889875175'
-const TELEGRAM_RECORDINGS_GROUP_ID = '-4862262334'
-
-const bitrateOptions = [
-  { value: 0, label: '24k' },
-  { value: 25, label: '56k' },
-  { value: 50, label: '96k' },
-  { value: 75, label: '128k' },
-  { value: 100, label: '196k' },
-];
+const TELEGRAM_OIL_OF_GLADNESS_ID = '-1002286427385'
+const TELEGRAM_SHEAF_YARD_GROUP_ID = '-1001388987517'
+const TELEGRAM_RECORDINGS_GROUP_ID = '-1002853673929'
 
 const telegramGroups = [
   { value: TELEGRAM_SHEAF_YARD_GROUP_ID, label: 'Sheaf Yard' },
@@ -51,13 +37,8 @@ const APP_STATES = {
 
 export default function QuickForm({ recordings, zoomToken }: { recordings: ZoomRecording[], zoomToken: string | undefined }) {
   const [status, setStatus] = useState('');
-  const [source, setSource] = useState('zoom-cloud');
   const [selectedRecording, setSelectedRecording] = useState<ZoomRecording>();
-  const [output, setOutput] = useState('telegram');
   const [group, setGroup] = useState<string>(TELEGRAM_SHEAF_YARD_GROUP_ID);
-  const [filters, setFilters] = useState('default');
-  const [volumeBoost, setVolumeBoost] = useState(1.6);
-  const [bitrate, setBitrate] = useState(50);
   const [appState, setAppState] = useState(APP_STATES.INIT);
   const [appErrorState, setAppErrorState] = useState<boolean>(false);
   
@@ -76,14 +57,11 @@ export default function QuickForm({ recordings, zoomToken }: { recordings: ZoomR
     },
 
     validate: {
-      audioFile: (value) => (source === 'upload' && !value ? 'File is required' : null),
       title: (value) => (value ? null : 'Title is required'),
-      date: (value) => (source === 'upload' && !value ? 'Date is required' : null),
     },
   });
 
-  const selectedDate = source === 'zoom-cloud' ? `${selectedRecording?.dateRaw}` : form.getValues().date;
-  const bitrateSubmitValue = bitrateOptions.find(v => v.value === bitrate)?.label;
+  const selectedDate = `${selectedRecording?.dateRaw}`;
 
   const handleZoomRecordingUpload = async (values: typeof form.values) => {
     const body = {
@@ -91,11 +69,9 @@ export default function QuickForm({ recordings, zoomToken }: { recordings: ZoomR
       date: selectedDate,
       downloadUrl: selectedRecording?.downloadUrl,
       zoomToken,
-      sendToTelegram: output === 'download' ? false : true,
+      sendToTelegram: true,
       groupId: group,
-      normalization: filters === 'custom' ? volumeBoost : undefined,
-      bitrate: filters === 'custom' && bitrateSubmitValue ? bitrateSubmitValue : undefined,
-      applyFilters: filters === 'no-filters' ? false : undefined,
+      applyFilters: true,
     };
 
     const res = await fetch(process.env.NEXT_PUBLIC_PROCESS_ZOOM_RECORDING as string, {
@@ -109,42 +85,13 @@ export default function QuickForm({ recordings, zoomToken }: { recordings: ZoomR
     return res;
   }
 
-  const handleFileUpload = async (values: typeof form.values) => {
-    const formData = new FormData();
-    formData.append('audio', values.audioFile as File);
-    formData.append('title', values.title);
-    formData.append('date', selectedDate);
-    formData.append('sendToTelegram', output === 'download' ? 'false' : 'true');
-    formData.append('groupId', group);
-    if(filters === 'custom') formData.append('normalization', volumeBoost.toString());
-    if(filters === 'custom' && bitrateSubmitValue) formData.append('bitrate', bitrateSubmitValue);
-    if(filters === 'no-filters') formData.append('applyFilters', 'false');
-
-    const res = await fetch(process.env.NEXT_PUBLIC_PROCESS_AUDIO_ENDPOINT as string, {
-      method: 'POST',
-      body: formData,
-    });
-
-    return res;
-  }
-
   const handleSubmit = async (values: typeof form.values) => {
     setAppState(APP_STATES.STARTED);
     setStatus('🚀 Uploading');
     
     try {
-      let res;
+      const res = await handleZoomRecordingUpload(values);
 
-      if(source === 'upload') res = await handleFileUpload(values);
-      else res = await handleZoomRecordingUpload(values);
-
-      if(output === 'download') {
-        setStatus(`📥 Converted successfully. Downloading....`);
-        await downloadFile(res, formatTitle(selectedDate, values.title));
-        setStatus(`✅ Downloaded successfully!`);
-
-        return;
-      }
 
       const data = await res.json();
 
